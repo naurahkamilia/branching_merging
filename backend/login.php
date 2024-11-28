6be25a8f8ca85adbc22cc0afeb3f444a22a33cbb
Naurah Kamilia <if23.naurahkamilia@mhs.ubpkarawang.ac.id>
2024-11-28 14:58:37 +0700

    Add files via upload

--- backend/login.php
+++ backend/login.php
@@ -1,13 +1,30 @@
 <?php
-    if(isset($_POST['email']) || isset($_POST['password'])){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
 
-        if($email == 'admin@admin.com' && $password == 'admin'){
-            header('Location: ./dashboard.php');
-        }else{
-            echo "Email atau Password salah.";
+require './../config/db.php';
+
+if(isset($_POST['submit'])) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = mysqli_query($db_connect,"SELECT * FROM users WHERE email = '$email'");
+    if(mysqli_num_rows($user) > 0) {
+        $data = mysqli_fetch_assoc($user);
+        
+        if(password_verify($password,$data['password'])) {
+            echo "selamat datang ".$data['name'];
+            die;
+
+            //otorisasi
+        } else {
+            echo "password salah";
+            die;
         }
-    }
 
-    ?>
+    } else {
+        echo "email atau password salah";
+        die;
+    }
+}
+
+?>
